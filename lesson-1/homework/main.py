718f83c42648a4fced95803cddd98154290c8cc8
Javlonboy <javlonboy09@gmail.com>
2025-10-20 23:03:36 +0500

    Create main.py

---
+++ lesson-1/homework/main.py
@@ -0,0 +1,30 @@
+# 1 - task
+# Given a side of square. Find its perimeter and area.
+side = 10
+perimetr = 4 * side
+area = side**2
+print("Perimetr:", perimetr,"Area:", area)
+
+# 2 - task
+# Given diameter of circle. Find its length.
+diameter = 10
+pi = 22/7
+length = pi*diameter
+print("Circle length: ", length)
+# 3 - task
+# Given two numbers a and b. Find their mean.
+a = 10
+b = 20
+mean = (a + b)/2
+print("Mean:", mean)
+# 4 - task
+# Given two numbers a and b. Find their sum, product and square of each number.
+a = 10
+b = 20
+sum = a + b
+product = a * b
+a_square = a ** 2
+b_square = b ** 2
+
+print("a,b sum equal to:", sum,"\nProduct a,b equal to:", product,"\na square equal to:", a_square,
+      "\nb square equal to:", b_square)
